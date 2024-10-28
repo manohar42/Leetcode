0f579aad5b257dc6e2ace301335700966e6971be
manohar42 <122593163+manohar42@users.noreply.github.com>
2024-10-27 19:32:54 -0700

    Time: 115 ms (59.46%), Space: 37.8 MB (12.33%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.py
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.py
@@ -5,16 +5,21 @@ class Solution:
         nums.sort()
         num_set = set(nums)
         max_size = -1
-
+        hashmap = set()
         for i in nums:
             k = i
             count = 1
-            while True:
-                k = k*k
-                if k in num_set:
-                    count+=1
-                else:
-                    break
+            if i not in hashmap:
+                hashmap.add(k)
+                while True:
+                    k = k*k
+                    if k in num_set:
+                        count+=1
+                        hashmap.add(k)
+                    else:
+                        break
+            else:
+                continue   
             if count >= 2:
                 max_size = max(max_size,count)
         return max_size
