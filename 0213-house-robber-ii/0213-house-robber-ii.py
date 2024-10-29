bc0e4df0d5ee7a250193b0d36d58a20e8d11e896
manohar42 <122593163+manohar42@users.noreply.github.com>
2024-10-29 16:00:25 -0700

    Time: 0 ms (100%), Space: 16.8 MB (6.5%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.py
+++ 0213-house-robber-ii/0213-house-robber-ii.py
@@ -24,5 +24,4 @@ class Solution:
             dp_1[i] = max(dp_1[i-1], dp_1[i-2]+nums[i])
         
         dp_1[n-1] = max(dp_1[n-2],dp_1[n-3]+nums[n-1])
-        print(dp,dp_1)
         return max(dp[n-2], dp_1[n-1])
