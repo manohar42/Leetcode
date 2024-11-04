e583d927dcfaa7df932a66d2bdcad826ffa34ccf
manohar42 <122593163+manohar42@users.noreply.github.com>
2024-11-04 12:02:21 -0700

    Time: 251 ms (49.65%), Space: 19 MB (85.91%) - LeetHub

---
+++ 3451-string-compression-iii/3451-string-compression-iii.py
@@ -0,0 +1,25 @@
+class Solution:
+    def compressedString(self, word: str) -> str:
+        
+        s = ""
+        n = len(word)
+        start = 0
+        count = 0
+        for i in range(0,n):
+            if i == 0:
+                count+=1
+
+            if i > 0:
+                if word[i] == word[i-1]:
+                    count+=1
+                else:
+                    s+=str(count)+word[i-1]
+                    count = 1
+                    # print(s)
+            if count > 9:
+                s+=str(count-1)+word[i-1]
+                count = count -9
+                # print(s)
+            # print(s)
+        s+=str(count)+word[-1]
+        return s
