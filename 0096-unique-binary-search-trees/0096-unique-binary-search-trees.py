7ea929ab67c444a2a3096c1825144eda1f0f8daa
manohar42 <122593163+manohar42@users.noreply.github.com>
2024-11-07 17:01:24 -0700

    Time: 0 ms (100%), Space: 16.7 MB (18.53%) - LeetHub

---
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numTrees(self, n: int) -> int:
+        
+        dp = [0]*(n+1)
+        dp[0]=1
+        dp[1]=1
+        if n ==  1:
+            return dp[1]
+        dp[2]=2
+
+        for i in range(3,n+1):  
+            count = 0
+            for j in range(1,i+1):
+                left = j-1
+                right = i-j
+                count+=(dp[left]*dp[right])
+            # p.rint(dp)
+            dp[i] = count
+            # print(dp)
+        return dp[n]
+
+
