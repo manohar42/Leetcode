1b7a3dc37b7ac8d161d184d90c026b7e794c50f2
manohar42 <122593163+manohar42@users.noreply.github.com>
2024-11-26 13:19:11 -0700

    Time: 15 ms (33.33%), Space: 18.4 MB (10.08%) - LeetHub

--- 3189-find-champion-ii/3189-find-champion-ii.py
+++ 3189-find-champion-ii/3189-find-champion-ii.py
@@ -1,18 +1,16 @@
 class Solution:
     def findChampion(self, n: int, edges: List[List[int]]) -> int:
         
-        hashmap = defaultdict()
-        for i in range(n):
-            hashmap[i] = 0
+        hashmap = [False]*n
         count = 0 
         for i in edges:
-            if hashmap[i[1]] == 0:
+            if hashmap[i[1]] == False:
                 count+=1
             hashmap[i[1]]+=1
         if count < n-1:
             return -1
-        for i in hashmap:
-            if hashmap[i] == 0:
+        for i in range(n):
+            if hashmap[i] == False:
                 return i
             
         
