9b366eb717578a5b120c9c00e015738c2716d6bb
manohar42 <122593163+manohar42@users.noreply.github.com>
2024-12-03 17:56:57 -0700

    Time: 75 ms (63.19%), Space: 17.9 MB (5.69%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.py
@@ -4,10 +4,10 @@ class Solution:
         if len(str2) > len(str1):
             return False
         
-        str_2_list = list(str2)
+        m = len(str2)-1
         n = len(str1)-1
-        while str_2_list and n >= 0:
-            k = str_2_list.pop()
+        while m>=0 and n >= 0:
+            k = str2[m]
             value = ord(str1[n])
             value_1 = value+1
             if value_1 > 122:
@@ -15,10 +15,11 @@ class Solution:
 
             if value == ord(k) or value_1 == ord(k):
                 n-=1
+                m-=1
                 continue
             n-=1
-            str_2_list.append(k)
-        # print(str_2_list)
-        if len(str_2_list) == 0:
+            
+        print(m)
+        if m < 0:
             return True
         return False
