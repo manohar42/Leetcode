2963040e4174538b9e4337a96afa3e2bed9b061e
manohar42 <122593163+manohar42@users.noreply.github.com>
2025-01-13 22:50:45 -0700

    Time: 53 ms (30.28%), Space: 18.1 MB (45.59%) - LeetHub

---
+++ 1502-construct-k-palindrome-strings/1502-construct-k-palindrome-strings.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canConstruct(self, s: str, k: int) -> bool:
+        
+        arr = [0 for _ in range(0,26)]
+        count = 0
+        n = len(s)
+        if n <k:
+            return False
+        for i in s:
+            # print(ord(i)-ord('a'))
+            arr[ord(i)-ord('a')] +=1
+        for j in arr:
+            if j%2 != 0:
+                count+=1
+        if count > k:
+            return False
+        return True
