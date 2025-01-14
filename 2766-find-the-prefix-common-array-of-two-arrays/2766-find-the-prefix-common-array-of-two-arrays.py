28da91568d85cc5ac65fc4e37714baddf03aad14
manohar42 <122593163+manohar42@users.noreply.github.com>
2025-01-13 18:58:08 -0700

    Time: 7 ms (75%), Space: 17.9 MB (19.53%) - LeetHub

--- 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/2766-find-the-prefix-common-array-of-two-arrays.py
@@ -7,15 +7,18 @@ class Solution:
         res = []
         for i in range(0,n):
             hashmap[A[i]] = hashmap.get(A[i],0)+1
+            
+            # if A[i] != B[i]:
+            #     if hashmap[A[i]] == 2:
+            #         count+=1
+            #     if hashmap[B[i]] == 2:
+            #         count+=1
+            # else:
+            if hashmap[A[i]] == 2:
+                    count+=1
             hashmap[B[i]] = hashmap.get(B[i],0)+1
-            if A[i] != B[i]:
-                if hashmap[A[i]] == 2:
-                    count+=1
-                if hashmap[B[i]] == 2:
-                    count+=1
-            else:
-                if hashmap[A[i]] == 2:
-                    count+=1
+            if hashmap[B[i]] == 2:
+                count+=1
             res.append(count)
         return res
                 
