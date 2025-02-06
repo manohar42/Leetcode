9c185e1a71aa88c758d7a5a0415d2d68d97129ba
manohar42 <122593163+manohar42@users.noreply.github.com>
2025-02-05 18:29:39 -0700

    Time: 346 ms (70.14%), Space: 46.5 MB (22.27%) - LeetHub

--- 1364-tuple-with-same-product/1364-tuple-with-same-product.py
+++ 1364-tuple-with-same-product/1364-tuple-with-same-product.py
@@ -6,12 +6,10 @@ class Solution:
         for i in range(0,n):
             for j in range(i+1,n):
                 k = nums[i]*nums[j]
-                hashmap[k] = hashmap.get(k,0) + 1
-                # print(hashmap)
+                hashmap[k] = hashmap.get(k,0) + 1      
         count = 0
         for i in hashmap:
             if hashmap[i] > 1:
-                number_of_ways = hashmap[i]*(hashmap[i]-1)
-                count += (number_of_ways*4)
-        # print(count)
+                number_of_ways = hashmap[i]*(hashmap[i]-1)*4
+                count += number_of_ways
         return count
