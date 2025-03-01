a53948a8ddad28b8744c29b704e3c1c1b5774eea
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-03-01 00:23:16 -0700

    Time: 1 ms (99.53%), Space: 46.3 MB (10.76%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -1,25 +1,20 @@
 class Solution {
 
     public void moveZeroes(int[] nums) {
-        ArrayList<Integer> alist = new ArrayList<Integer>();
+        int pos = 0;
 
-        int left = 0;
-        int right = 0;
-        for(int i = 0;i<nums.length;i++){
-            if(nums[i] == 0){
-                alist.add(i);
-            }
-            else{
-                if(alist.size() > 0){
-                    // int k = ;
-                    nums[alist.get(0)] = nums[i];
-                    alist.remove(0);
-                    nums[i] = 0;
-                    alist.add(i);
-                }
+        for(int i:nums){
+            if(i!= 0){
+                nums[pos] = i;
+                pos++; 
             }
         }
-        
+        while(pos<nums.length){
+            nums[pos] = 0;
+            pos++;
+        }
+    
+
 
     }
 }
