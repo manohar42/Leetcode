503159f3c8e83c0a2b7c295cb9da8cde8bad3dfb
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-03-04 20:00:20 -0700

    Time: 104 ms (33.83%), Space: 17.9 MB (8.65%) - LeetHub

--- 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
+++ 2649-count-total-number-of-colored-cells/2649-count-total-number-of-colored-cells.py
@@ -2,13 +2,10 @@ class Solution:
     def coloredCells(self, n: int) -> int:
 
         row_count = n*2-1
-        colored_rows = 1
         number_of_colored_cells= 0
-        # i = 1
-        while(colored_rows<row_count):
-            number_of_colored_cells+=colored_rows
-            colored_rows+=2
-            # i+=1
+        for i in range(row_count-2,-1,-2):
+            number_of_colored_cells+=i
+
         return number_of_colored_cells*2+row_count
 
 
