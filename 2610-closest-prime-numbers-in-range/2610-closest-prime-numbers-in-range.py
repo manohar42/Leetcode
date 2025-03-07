9cb65d486cab1e5f39a7bd12037524f1bf71d056
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-03-06 18:33:18 -0700

    Time: 7849 ms (5%), Space: 77.6 MB (5.31%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.py
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.py
@@ -1,25 +1,37 @@
 class Solution:
     def closestPrimes(self, left: int, right: int) -> List[int]:
 
-        def isPrime(n):
-            if n <= 1:
-                return False
-            if n<=3:
-                return True
-            if n%2 == 0 or n%3==0:
-                return False
-            for j in range(5, int(math.sqrt(n))+1,6):
-                if n%j == 0 or n%(j+2) == 0:
-                    return False
-            return True
+        def isPrimes(n):
+            isPrime = [True for i in range(0,n)]
+            primes = []
+            isPrime[0] =False
+            isPrime[1] = False
+            for i in range(2,n):
+                primes.append(i)
+                for j in range(i*i,n,i):
+                    isPrime[j] = False
+            return isPrime
 
         min_diff = float('inf') 
         res = [-1,-1]
         prev_prime = 0
+        # for i in range(left,right+1):
+        #     if (i%2 == 0 and i >2) or (i%3 == 0  and i > 3):
+        #         continue
+        #     if isPrime(i):
+        #         if prev_prime == 0:
+        #             prev_prime = i
+        #             continue
+        #         diff = i - prev_prime
+        #         if diff < min_diff:
+        #             min_diff = diff
+        #             res = [prev_prime,i]
+        #         prev_prime = i
+        # return res                
+        primes = isPrimes(right+1)
+
         for i in range(left,right+1):
-            if (i%2 == 0 and i >2) or (i%3 == 0  and i > 3):
-                continue
-            if isPrime(i):
+            if primes[i] == True:
                 if prev_prime == 0:
                     prev_prime = i
                     continue
@@ -28,5 +40,4 @@ class Solution:
                     min_diff = diff
                     res = [prev_prime,i]
                 prev_prime = i
-        return res                
-                
+        return res        
