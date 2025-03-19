5ddbb675087266d0f48230daf61700a385b71105
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-03-18 17:27:20 -0700

    Time: 115 ms (55.27%), Space: 21.6 MB (54.5%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -6,8 +6,6 @@ class Solution:
             if nums[i] == 0:
                 count+=1
                 nums[i]=1
-                nums[i+1] = 1 if nums[i+1] == 0 else 0
-                nums[i+2] = 1 if nums[i+2] == 0 else 0
-        #     print(nums)
-        # print(nums)
+                nums[i+1] = nums[i+1]^1
+                nums[i+2] = nums[i+2]^1
         return count if len(nums)==sum(nums) else -1
