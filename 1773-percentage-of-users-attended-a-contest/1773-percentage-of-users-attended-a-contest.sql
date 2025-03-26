b98773d173173e8412608ea5e46a95e7352b4706
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-03-25 21:05:54 -0700

    Time: 972 ms (94.56%), Space: 0B (100%) - LeetHub

--- 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/1773-percentage-of-users-attended-a-contest.sql
@@ -3,4 +3,4 @@
 select contest_id, round(count(user_id)*100  /(select count(user_id) from Users),2)as percentage
 from Register
 group by contest_id
-order by round(count(user_id)*100  /(select count(user_id) from Users),2) desc, contest_id asc
+order by percentage desc, contest_id asc
