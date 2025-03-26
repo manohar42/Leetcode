d8671cb79ce68edb1ac7da2d02f419e79716c638
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-03-26 00:47:00 -0700

    Time: 609 ms (97.5%), Space: 0B (100%) - LeetHub

---
+++ 1292-immediate-food-delivery-ii/1292-immediate-food-delivery-ii.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+
+with tab as (select *, Row_Number() over(partition by customer_id order by order_date) as rn
+from Delivery
+order by customer_id asc,order_date asc)
+
+-- select count()
+select round(((select count(*) from tab where order_date = customer_pref_delivery_date and rn = 1 )*100/(
+    select count(distinct customer_id) from Delivery)),2) as immediate_percentage
