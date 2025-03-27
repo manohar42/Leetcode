8c9d3a263f10385a9952aa61ad8076f1cd4a763c
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-03-27 15:07:30 -0700

    Time: 917 ms (99.22%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,18 +1,18 @@
 # Write your MySQL query statement below
 
--- select p.product_id, s.year as first_year,sum(s.quantity) as quantity, s.price
--- from Product p join Sales s
--- on p.product_id = s.product_id
--- where (p.product_id,s.year)
--- in (select product_id,min(year) from Sales group by product_id )
--- order by p.product_id asc
+select p.product_id, s.year as first_year,s.quantity as quantity, s.price
+from Product p join Sales s
+on p.product_id = s.product_id
+where (p.product_id,s.year)
+in (select product_id,min(year) from Sales group by product_id )
+order by p.product_id asc
 
-with cte as(
-    select product_id,min(year) as min_year from Sales
-    group by product_id
-)
+-- with cte as(
+--     select product_id,min(year) as min_year from Sales
+--     group by product_id
+-- )
 
-select s.product_id, s.year as first_year,s.quantity as quantity, s.price
-from sales s join
-cte as c on c.product_id = s.product_id 
-and c.min_year = s.year
+-- select s.product_id, s.year as first_year,s.quantity as quantity, s.price
+-- from sales s join
+-- cte as c on c.product_id = s.product_id 
+-- and c.min_year = s.year
