d53753d9a9421939cc1d0a7380abe77d0a57f6f3
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-04-04 09:08:15 -0700

    Time: 0 ms (100%), Space: 18 MB (23.03%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.py
+++ 0029-divide-two-integers/0029-divide-two-integers.py
@@ -1,12 +1,18 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        sign = -1 if (dividend >= 0 and divisor < 0) or (dividend < 0 and divisor >= 0) else 1
-        dividend = abs(dividend)
-        divisor = abs(divisor)
-        result = len(range(0, dividend-divisor+1, divisor))
-        if sign == -1:
-            result = -result
-        minus_limit = -(2**31)
-        plus_limit = (2**31 - 1)
-        result = min(max(result, minus_limit), plus_limit)
-        return result
+
+    
+        
+        a = abs(dividend)
+        b = abs(divisor)
+    
+        if (dividend<0 and divisor >=0) or (divisor<0 and dividend >= 0):
+            sign = -1
+        else:
+            sign = 1
+       
+        q = len(range(0,a-b+1,b))
+        minus_limit = (-(2**31)-1)
+        # print(minus_limit)
+        plus_limit = (2**31-1)
+        return min(max(minus_limit,q*sign),plus_limit)
