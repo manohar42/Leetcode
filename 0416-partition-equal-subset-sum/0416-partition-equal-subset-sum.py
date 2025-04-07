1ec82b040678775bdbb0ead565323709262f2976
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-04-06 20:22:26 -0700

    Time: 323 ms (84.67%), Space: 107.6 MB (18.46%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,47 +1,25 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         
-        # if sum(nums)%2!=0:
-        #     return False
-        # sum_value = sum(nums)//2
-        # dp = set()
-        # dp.add(0)
-
-        # def recursion(nums,dp,i):
-        #     if index == -1:
-        #         return False
-        #     sub_dp = set()
-        #     for t in dp:
-        #         sub_dp.add(t+nums[i])
-        #         sub_dp.add(t)
-        #         if t+nums[i]== sum_value:
-        #             return True
-        #     dp = sub_dp
-        #     return recursion(nums,dp,i-1)
-            
-        # return recursion(nums,dp,len(nums)-1)
-        if sum(nums)%2 != 0:
+        if sum(nums)%2!=0:
             return False
+        sum_value = sum(nums)//2
         dp = set()
         dp.add(0)
 
-        n =len(nums)-1
-        target = sum(nums)//2
-        def recursion(nums,dp,index):
-
-            if index == -1:
+        def recursion(nums,dp,i):
+            if i == -1:
                 return False
-            
             sub_dp = set()
             for t in dp:
-                sub_dp.add(t+nums[index])
+                sub_dp.add(t+nums[i])
                 sub_dp.add(t)
-                if t+nums[index] == target:
+                if t+nums[i]== sum_value:
                     return True
             dp = sub_dp
-            return recursion(nums,dp,index-1)
-
-
-
+            return recursion(nums,dp,i-1)
+            
+        return recursion(nums,dp,len(nums)-1)
         
-        return recursion(nums,dp,n)
+
+    
