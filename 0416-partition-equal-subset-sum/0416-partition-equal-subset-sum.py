c1402b6c9ded46c856ff620f96280b22b04ac6ee
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-04-06 20:17:59 -0700

    Time: 320 ms (84.75%), Space: 107.7 MB (18.44%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,23 +1,47 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         
-        if sum(nums)%2!=0:
-            return False
-        sum_value = sum(nums)//2
+        # if sum(nums)%2!=0:
+        #     return False
+        # sum_value = sum(nums)//2
+        # dp = set()
+        # dp.add(0)
 
-        def recursion(nums,i,sum_value):
-            if sum_value == 0:
-                return True
-
-            if i == 0:
-                return False
-
-            if nums[i-1] > sum_value:
-                return recursion(nums,i-1,sum_value)
+        # def recursion(nums,dp,i):
+        #     if index == -1:
+        #         return False
+        #     sub_dp = set()
+        #     for t in dp:
+        #         sub_dp.add(t+nums[i])
+        #         sub_dp.add(t)
+        #         if t+nums[i]== sum_value:
+        #             return True
+        #     dp = sub_dp
+        #     return recursion(nums,dp,i-1)
             
-            return recursion(nums,i-1,sum_value-nums[i-1]) or recursion(nums,i-1,sum_value)
-        
-        return recursion(nums,len(nums),sum_value)
-        
+        # return recursion(nums,dp,len(nums)-1)
+        if sum(nums)%2 != 0:
+            return False
+        dp = set()
+        dp.add(0)
 
-    
+        n =len(nums)-1
+        target = sum(nums)//2
+        def recursion(nums,dp,index):
+
+            if index == -1:
+                return False
+            
+            sub_dp = set()
+            for t in dp:
+                sub_dp.add(t+nums[index])
+                sub_dp.add(t)
+                if t+nums[index] == target:
+                    return True
+            dp = sub_dp
+            return recursion(nums,dp,index-1)
+
+
+
+        
+        return recursion(nums,dp,n)
