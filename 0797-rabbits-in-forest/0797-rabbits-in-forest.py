0feff0b4dcd8c873a632dd9ae116edef5a8ec219
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-04-19 17:21:48 -0700

    Time: 4 ms (11.67%), Space: 17.9 MB (76.07%) - LeetHub

---
+++ 0797-rabbits-in-forest/0797-rabbits-in-forest.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numRabbits(self, answers: List[int]) -> int:
+        
+
+        hashmap = defaultdict()
+        min_count = 0
+        for i in range(0,len(answers)):
+            hashmap[answers[i]] = hashmap.get(answers[i],0)+1
+            if answers[i]+1 == hashmap[answers[i]]:
+                min_count+= hashmap[answers[i]]
+                hashmap[answers[i]] = 0
+        # print(hashmap)
+        # print(min_count)
+        for i in hashmap:
+            if hashmap[i]!=0:
+                min_count+=i+1
+        return min_count
