b80f207ab6cd21280b6c036618b92737b8333d47
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-04-27 08:29:30 -0700

    Time: 3 ms (91.43%), Space: 17.6 MB (92.86%) - LeetHub

--- 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.py
+++ 3685-count-subarrays-of-length-three-with-a-condition/3685-count-subarrays-of-length-three-with-a-condition.py
@@ -1,9 +1,10 @@
 class Solution:
     def countSubarrays(self, nums: List[int]) -> int:
         
+        right = 2
         count = 0
         for i in range(0,len(nums)-2):
             if (nums[i]+nums[i+2])*2 == nums[i+1]:
                 count+=1
-
+            right+=1
         return count 
