dee23555a3f0b8f8722e5d0611d588392b9d75e5
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-05-10 17:26:29 -0700

    Time: 0 ms (100%), Space: 18 MB (38.15%) - LeetHub

--- 1293-three-consecutive-odds/1293-three-consecutive-odds.py
+++ 1293-three-consecutive-odds/1293-three-consecutive-odds.py
@@ -4,9 +4,14 @@ class Solution:
         
         n = len(arr)
         i=0
+        count = 0
         while i < n:
-            if arr[i]%2!=0 and i+2 < n:
-                if arr[i+1]%2!=0 and arr[i+2]%2!=0:
-                    return True
+            if arr[i] &1 == 1:
+                count+=1
+            else:
+                count =0
+            if count ==3:
+                return True
             i+=1
+
         return False
