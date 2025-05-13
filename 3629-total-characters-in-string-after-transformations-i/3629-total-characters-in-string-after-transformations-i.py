266072775b131b99544036dc00dd9011882f7393
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-05-12 18:49:18 -0700

    Time: 11676 ms (5.23%), Space: 18.5 MB (35.29%) - LeetHub

---
+++ 3629-total-characters-in-string-after-transformations-i/3629-total-characters-in-string-after-transformations-i.py
@@ -0,0 +1,33 @@
+class Solution:
+    def lengthAfterTransformations(self, s: str, t: int) -> int:
+        
+
+
+        
+        mod = (10**9)+7
+        hashmap = [0]*26
+        for i in s:
+            hashmap[ord(i)-ord('a')] +=1   
+        # print(hashmap)     
+        
+        for i in range(0,t):    
+            NewMap = hashmap.copy()
+            for j in range(0,26):
+                if hashmap[j] > 0:
+                    NewMap[j]-=hashmap[j]
+                    if (j+1)%26 == 0:
+                        NewMap[0]+=hashmap[j]
+                        NewMap[1]+=hashmap[j]
+                    else:
+                        NewMap[j+1] +=hashmap[j]
+                    # hashmap[j] = NewMap[j]
+        
+            hashmap = NewMap.copy()
+        
+        res = 0
+        # print(hashmap)
+        for i in hashmap:
+            res+=i
+            res = res%(mod)
+        return res
+            
