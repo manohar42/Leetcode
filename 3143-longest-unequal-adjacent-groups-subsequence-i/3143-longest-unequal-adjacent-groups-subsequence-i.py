f7ee5aa655546c52f22d631d269bfc61a3062b6f
Manohar Veeravalli <122593163+manohar42@users.noreply.github.com>
2025-05-15 07:29:06 -0700

    Time: 3 ms (28.03%), Space: 17.7 MB (65.57%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.py
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.py
@@ -5,7 +5,6 @@ class Solution:
         sequence_1 = []
         value = 0
         n = len(groups)
-        print(n)
         for i in range(0,n):
             if value == groups[i]:
                 sequence_0.append(i)
@@ -13,18 +12,14 @@ class Solution:
                     value = 0
                 else:
                     value = 1
-        # print(sequence_0)
         value = 1
         for i in range(0,n):
-            # print(value,groups[i])
             if value == groups[i]:
                 sequence_1.append(i)
                 if value ==1:
                     value = 0
                 else:
                     value = 1
-        # print(sequence_1)
-
         sequence = sequence_1 if len(sequence_1) > len(sequence_0) else sequence_0
 
         res = []
