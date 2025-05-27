cfa3f12157d5546d628c5b611fed36311a9e5b95
Manohar <122593163+manohar42@users.noreply.github.com>
2025-05-26 17:25:18 -0700

    Time: 0 ms (100%), Space: 17.8 MB (67.05%) - LeetHub

--- 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.py
+++ 3172-divisible-and-non-divisible-sums-difference/3172-divisible-and-non-divisible-sums-difference.py
@@ -3,7 +3,9 @@ class Solution:
         
         sum_value = (n*(n+1))//2
     
-        for i in range(1,n+1):
-            if i%m == 0:
-                sum_value-= (2*i)
-        return sum_value
+        value = 0
+        res = 0
+        while res+m <= n:
+            res +=m
+            value+=res
+        return sum_value - (2*value)
