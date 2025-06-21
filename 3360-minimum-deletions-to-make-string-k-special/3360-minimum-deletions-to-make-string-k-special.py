2bcbadc0adf9ceaf2cf66e63e87a67a1564114c1
Manohar <122593163+manohar42@users.noreply.github.com>
2025-06-21 09:14:40 -0700

    Time: 102 ms (13.18%), Space: 18.2 MB (45.74%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.py
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.py
@@ -7,19 +7,20 @@ class Solution:
         c_count = 0
         s.sort()
         min_count = len(word)
-        for i in range(0,26):
-            c_count =0
+        for i in range(0,26): 
             min_val = s[i]
-            for j in range(0,26):
-                if s[j]!=0 and i!=j:
-                    if s[j] < min_val:
-                        c_count+=s[j]
-                    elif s[j]-min_val <=k:
-                        continue
-                    else:
-                        c_count+=s[j]-min_val-k
-            if c_count < min_count:
-                min_count = c_count
+            if s[i] > 0:
+                c_count =0
+                for j in range(0,26):
+                    if s[j]!=0 and i!=j:
+                        if s[j] < min_val:
+                            c_count+=s[j]
+                        elif s[j]-min_val <=k:
+                            continue
+                        else:
+                            c_count+=s[j]-min_val-k
+                if c_count < min_count:
+                    min_count = c_count
         return min_count
                 
 
