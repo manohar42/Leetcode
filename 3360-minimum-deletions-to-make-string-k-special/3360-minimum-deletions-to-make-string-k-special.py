fe8ffd83f60af4eb6b30ac32b9bcbb525305aa21
Manohar <122593163+manohar42@users.noreply.github.com>
2025-06-21 09:18:15 -0700

    Time: 95 ms (24.81%), Space: 18 MB (70.54%) - LeetHub

--- 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.py
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.py
@@ -6,19 +6,20 @@ class Solution:
             s[ord(i)-ord('a')] +=1
         c_count = 0
         s.sort()
+        prefix_sum = 0
         min_count = len(word)
         for i in range(0,26): 
             min_val = s[i]
+            prefix_sum+=s[i-1] if i > 0 else 0
             if s[i] > 0:
-                c_count =0
-                for j in range(0,26):
-                    if s[j]!=0 and i!=j:
-                        if s[j] < min_val:
-                            c_count+=s[j]
-                        elif s[j]-min_val <=k:
-                            continue
-                        else:
-                            c_count+=s[j]-min_val-k
+                c_count = prefix_sum
+                for j in range(i+1,26):
+                    if s[j] < min_val:
+                        c_count+=s[j]
+                    elif s[j]-min_val <=k:
+                        continue
+                    else:
+                        c_count+=s[j]-min_val-k
                 if c_count < min_count:
                     min_count = c_count
         return min_count
