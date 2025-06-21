68c8f9748eba94834304e5bbda7341910af3d976
Manohar <122593163+manohar42@users.noreply.github.com>
2025-06-21 09:12:41 -0700

    Time: 111 ms (10.85%), Space: 18 MB (70.54%) - LeetHub

---
+++ 3360-minimum-deletions-to-make-string-k-special/3360-minimum-deletions-to-make-string-k-special.py
@@ -0,0 +1,26 @@
+class Solution:
+    def minimumDeletions(self, word: str, k: int) -> int:
+        
+        s = [0 for i in range(0,26)]
+        for i in word:
+            s[ord(i)-ord('a')] +=1
+        c_count = 0
+        s.sort()
+        min_count = len(word)
+        for i in range(0,26):
+            c_count =0
+            min_val = s[i]
+            for j in range(0,26):
+                if s[j]!=0 and i!=j:
+                    if s[j] < min_val:
+                        c_count+=s[j]
+                    elif s[j]-min_val <=k:
+                        continue
+                    else:
+                        c_count+=s[j]-min_val-k
+            if c_count < min_count:
+                min_count = c_count
+        return min_count
+                
+
+
