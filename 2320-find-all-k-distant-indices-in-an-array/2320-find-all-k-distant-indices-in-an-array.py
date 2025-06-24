4696dad2bbc999c78672e99669b1008e20d8bb4f
Manohar <122593163+manohar42@users.noreply.github.com>
2025-06-24 09:28:37 -0700

    Time: 230 ms (24.24%), Space: 17.9 MB (68.6%) - LeetHub

--- 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.py
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.py
@@ -8,16 +8,10 @@ class Solution:
                 sub.append(i)
         n = len(nums)
         i = 0
-        for i in sub:
-            while i < n:
-                if nums[i] == key:
-                    left = max(0,i-k)
-                    right = min(n-1,abs(i+k))
-                    # print(left,right)
-                    while left <= right:
-                        res.add(left)
-                        left+=1
-                    i = right+1
-                else:
-                    i+=1
+        for i in sub:       
+            left = max(0,i-k)
+            right = min(n-1,abs(i+k))
+            while left <= right:
+                res.add(left)
+                left+=1
         return list(res)
