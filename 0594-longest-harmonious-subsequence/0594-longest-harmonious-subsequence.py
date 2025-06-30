9d917f93c34d290906dbb9680633c6d28f01da81
Manohar <122593163+manohar42@users.noreply.github.com>
2025-06-29 18:37:55 -0700

    Time: 47 ms (31.79%), Space: 19.2 MB (21.92%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.py
@@ -2,12 +2,11 @@ class Solution:
     def findLHS(self, nums: List[int]) -> int:
         
         hashmap = Counter(nums)
-        print(hashmap)
         max_sequence = 0
         sequence_count = 0
-        for i in range(0,len(nums)):
-            if max(hashmap.get(nums[i]-1,0),hashmap.get(nums[i]+1,0)):
-                sequence_count = max(hashmap.get(nums[i]-1,0),hashmap.get(nums[i]+1,0))+hashmap[nums[i]]
+        for i in hashmap:
+            if max(hashmap.get(i-1,0),hashmap.get(i+1,0)):
+                sequence_count = max(hashmap.get(i-1,0),hashmap.get(i+1,0))+hashmap[i]
                 max_sequence = max(sequence_count,max_sequence)
         
         return max_sequence
