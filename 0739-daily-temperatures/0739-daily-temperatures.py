5b1c62e783b1c3b101114f90987b2e0aa0ec2a4f
Manohar <122593163+manohar42@users.noreply.github.com>
2026-05-23 00:01:51 -0700

    Time: 133 ms (11.82%), Space: 34.4 MB (23.67%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -3,14 +3,10 @@ class Solution:
         stack = []
 
         res = [0]*len(temperatures)
-
-        for i in range(0,len(temperatures)):
-            if i == 0:
-                stack.append((temperatures[i],i))
-                continue
+        stack.append((temperatures[0],0))
+        for i in range(1,len(temperatures)):
             while stack and stack[-1][0] < temperatures[i]:
                 res[stack[-1][1]] = i-stack[-1][1]
-                
                 stack.pop()
             stack.append((temperatures[i],i))
         
