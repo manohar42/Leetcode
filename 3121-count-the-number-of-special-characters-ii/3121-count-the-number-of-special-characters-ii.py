bbac140f6fe83f9e40548ea576010664367d3f9a
Manohar <122593163+manohar42@users.noreply.github.com>
2026-05-27 20:55:29 -0700

    Time: 269 ms (49.31%), Space: 21.7 MB (26.04%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.py
@@ -7,17 +7,12 @@ class Solution:
         hashmap = {}
 
         for i in range(0,len(word)):
-
             if ord(word[i]) in hashmap:
                 hashmap[ord(word[i])][1] = i
             else:
                 hashmap[ord(word[i])] = [i,i]
-        # print(hashmap)
         for i in hashmap:
-            
             if i <=90 and i+32 in hashmap and hashmap[i][0] > hashmap[i+32][1]:
                 count+=1
-                # print(ord(word[i]))
                
-
         return count
