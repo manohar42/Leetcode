ac83870ddb42e30d0c89a27938f0cdcc938bb59a
Manohar <122593163+manohar42@users.noreply.github.com>
2026-05-28 17:55:45 -0700

    Time: 3 ms (80.35%), Space: 19.3 MB (21.61%) - LeetHub

--- 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
+++ 3300-minimum-element-after-replacement-with-digit-sum/3300-minimum-element-after-replacement-with-digit-sum.py
@@ -1,10 +1,7 @@
 class Solution:
     def minElement(self, nums: List[int]) -> int:
-        
-
 
         def sumofdigits(n):
-
             res = 0
             while n>=10:
                 res+=n%10
@@ -17,5 +14,7 @@ class Solution:
             nums[i] = sumofdigits(nums[i])
             if nums[i] < min_value:
                 min_value=nums[i]
+                if min_value == 1:
+                    return min_value
             
         return min_value
