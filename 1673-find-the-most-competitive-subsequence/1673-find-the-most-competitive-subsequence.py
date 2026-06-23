c96d65990d6876ee564ad8a83bdf193ea4207c82
Manohar <122593163+manohar42@users.noreply.github.com>
2026-06-22 20:38:00 -0700

    Time: 171 ms (5.12%), Space: 30.9 MB (88.2%) - LeetHub

---
+++ 1673-find-the-most-competitive-subsequence/1673-find-the-most-competitive-subsequence.py
@@ -0,0 +1,17 @@
+class Solution:
+    def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
+        min_que = deque()
+
+        for i in range(0,len(nums)):
+            # print(len(nums)-i-1,k-len(min_que))
+            while min_que and len(nums)-i-1>=k-len(min_que) and nums[i] < min_que[-1]:
+                min_que.pop()
+            if len(min_que) < k:
+                min_que.append(nums[i])
+        
+        res = []
+
+        while len(min_que)>0:
+            res.append(min_que.popleft())
+
+        return res
